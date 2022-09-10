0190f3b1c935b40bb79cdaec2964b0fefb4f24a1
theamazing0 <66532288+theamazing0@users.noreply.github.com>
2022-09-10 17:50:01 -0400

    Update configure.py

--- app/configure.py
+++ app/configure.py
@@ -3,11 +3,11 @@
 #* AssemblyAI API Key:
 assemblyai_key="API KEY GOES HERE"
 # Instructions can be found in the
-# "Get your AssemblyAI API Key" section of
+# 'Get your AssemblyAI API Key' section of
 # this article: https://www.assemblyai.com/blog/real-time-speech-recognition-with-python/
 
 #* DeepL Translation API Key
 deepl_key="API KEY GOES HERE"
-# Requires Sign Up To "DeepL Pro
+# Requires Sign Up To DeepL Pro
 # DeepL Pro has free 500k words
 # Sign Up Here: https://www.deepl.com/pro-account
